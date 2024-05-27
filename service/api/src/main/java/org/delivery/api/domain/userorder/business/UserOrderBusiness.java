65f99a2b7c72354e5d75e9a184e2b0d57497acff
hyun1994 <chaeji1994@gmail.com>
2024-05-28 01:12:24 +0900

    delivery asynchronous dev

--- service/api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
+++ service/api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
@@ -11,6 +11,7 @@ import org.delivery.api.domain.userorder.controller.model.UserOrderDetailRespons
 import org.delivery.api.domain.userorder.controller.model.UserOrderRequest;
 import org.delivery.api.domain.userorder.controller.model.UserOrderResponse;
 import org.delivery.api.domain.userorder.converter.UserOrderConverter;
+import org.delivery.api.domain.userorder.producer.UserOrderProducer;
 import org.delivery.api.domain.userorder.service.UserOrderService;
 import org.delivery.api.domain.userordermenu.converter.UserOrderMenuConverter;
 import org.delivery.api.domain.userordermenu.service.UserOrderMenuService;
@@ -34,6 +35,8 @@ public class UserOrderBusiness {
     private final StoreService storeService;
     private final StoreConverter storeConverter;
 
+    private final UserOrderProducer userOrderProducer;
+
     // 1. 사용자, 메뉴id
     // 2. userOrder 생성
     // 3. userOrderMenu 생성
@@ -56,6 +59,9 @@ public class UserOrderBusiness {
 
         // 주문내역 기록
         userOrderMenuEntityList.forEach(userOrderMenuService::order);
+
+        // 비동기로 가맹점에 주문 알리기
+        userOrderProducer.sendOrder(newUserOrderEntity);
 
         return userOrderConverter.toResponse(newUserOrderEntity);
     }
